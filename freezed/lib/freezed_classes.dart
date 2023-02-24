004ddec768f47e55cedc6a68faa3cd8fe8ab0bd8
rabindraacharya <rabindra_acharya2000@hotmail.com>
2023-02-24 12:22:31 +0545

    added inline comment

--- freezed/lib/freezed_classes.dart
+++ freezed/lib/freezed_classes.dart
@@ -2,11 +2,24 @@ import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'freezed_classes.freezed.dart';
+
 part 'freezed_classes.g.dart'; // Json Serializable code will be generated into seperate file
 
 @freezed
-@immutable
-abstract class User with _$User{
-    const factory User(String name, int age) = _User;
-    factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+abstract class User with _$User {
+  const factory User(String name, int age) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
+
+/**
+    The following snippet defines a model named User:
+
+    - User has 2 properties: name and age
+    - Because we are using @freezed, all of this class's properties are immutable.
+    - Since we defined a fromJson, this class is de/serializable. Freezed will add a toJson method for us.
+    - Freezed will also automatically generate:
+       - a copyWith method, for cloning the object with different properties
+       - a toString override listing all the properties of the object
+       - an operator == and hashCode override (since User is immutable)
+ */
